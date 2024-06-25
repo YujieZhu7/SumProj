0c1b08bdff57f82980b5f8ebcce4f18130758bdb
YujieZhu7 <156827930+YujieZhu7@users.noreply.github.com>
2024-06-25 21:19:15 +0100

    Update QFilter_test_dropout_first.py

--- Push/Push_Qfilter/MCdropout/QFilter_test_dropout_first.py
+++ Push/Push_Qfilter/MCdropout/QFilter_test_dropout_first.py
@@ -237,7 +237,4 @@ while steps < max_steps + 1:
             print("Acceptance Rate of Demos = %.2f " % (np.mean(last_ten_percent_demos)))
 
     episodes += 1
-# np.save(f"/home/zhu_y@WMGDS.WMG.WARWICK.AC.UK/PycharmProjects/pythonProject/Results/{env_name}/DropoutQfilter/RandGausNoise/{method}/0.5+1EnsSize_{ensemble_size}_S{seed}_score", score_history)
-# np.save(f"/home/zhu_y@WMGDS.WMG.WARWICK.AC.UK/PycharmProjects/pythonProject/Results/{env_name}/DropoutQfilter/RandGausNoise/{method}/0.5+1EnsSize_{ensemble_size}_S{seed}_success", success_history)
-# np.save(f"/home/zhu_y@WMGDS.WMG.WARWICK.AC.UK/PycharmProjects/pythonProject/Results/{env_name}/DropoutQfilter/RandGausNoise/{method}/0.5+1EnsSize_{ensemble_size}_S{seed}_demoaccept",
-#         average_accept_demos)
+
