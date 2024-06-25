43324bd5751a3211e71fc295a3c436a247059e58
YujieZhu7 <156827930+YujieZhu7@users.noreply.github.com>
2024-06-25 21:15:14 +0100

    Update QFilter_test_dropout_first.py

--- Push/Push_Qfilter/MCdropout/QFilter_test_dropout_first.py
+++ Push/Push_Qfilter/MCdropout/QFilter_test_dropout_first.py
@@ -240,4 +240,4 @@ while steps < max_steps + 1:
 # np.save(f"/home/zhu_y@WMGDS.WMG.WARWICK.AC.UK/PycharmProjects/pythonProject/Results/{env_name}/DropoutQfilter/RandGausNoise/{method}/0.5+1EnsSize_{ensemble_size}_S{seed}_score", score_history)
 # np.save(f"/home/zhu_y@WMGDS.WMG.WARWICK.AC.UK/PycharmProjects/pythonProject/Results/{env_name}/DropoutQfilter/RandGausNoise/{method}/0.5+1EnsSize_{ensemble_size}_S{seed}_success", success_history)
 # np.save(f"/home/zhu_y@WMGDS.WMG.WARWICK.AC.UK/PycharmProjects/pythonProject/Results/{env_name}/DropoutQfilter/RandGausNoise/{method}/0.5+1EnsSize_{ensemble_size}_S{seed}_demoaccept",
-        average_accept_demos)
+#         average_accept_demos)
